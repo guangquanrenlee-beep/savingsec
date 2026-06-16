6d443241d6326667dcc817c4c12bf6940b5b8347
ProductPick <admin@chanpintuijian.com>
2026-06-16 16:01:37 +0800

    更新: 周二 2026/06/16 16:01:37.68

---
+++ deploy.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 65001 >nul
+echo [1/3] Building...
+cd /d D:\site\chanpintuijian
+hugo --gc --minify
+if errorlevel 1 (
+    echo Build failed!
+    pause
+    exit /b 1
+)
+echo [2/3] Committing...
+git add -A
+git commit -m "更新: %date% %time%"
+echo [3/3] Pushing...
+git push
+if errorlevel 1 (
+    echo Push failed!
+    pause
+    exit /b 1
+)
+echo.
+echo Deployed! Cloudflare will update in 1-2 minutes.
+pause
